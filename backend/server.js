b9bb44aff96a53dc57d01851010152fa055bc955
KL-Coders0103 <lovesh.m.bodhani@gmail.com>
2026-03-10 15:45:22 +0530

    Added Mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const dns = require("dns");
 require("dotenv").config();
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// connect database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Prismora API Running");
